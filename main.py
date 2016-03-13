2bc3c69a36173e98fec9c832a54450004f8f1935
Elli Thomas <elli.thomas@gmail.com>
2016-03-13 21:53:56 +0000

    defined the results of the twitter coordinates as a variable and linked it to the template in javascript

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ import unicodecsv
 
 #Use form on template to collect search term
 
-app = Flask("MyApp")
+app = Flask(__name__)
+
 
 app.debug = True
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    tweetcoordinates = pull_tweets()
+    return render_template("index.html", templatecoordinates=tweetcoordinates)
+
+
+
+
+
 
 @app.route("/", methods=['GET'])
 def sign_up():
@@ -24,36 +31,43 @@ def sign_up():
 
    
 
-#send search term to tweepy to return tweets and geolocation
+#define function which sends search term to tweepy to return tweets and geolocation
 
-auth = tweepy.OAuthHandler('rsPQog0xWeNirfT5Bmx9M9CGy', 'S3lmK37dlt7ZNsRIxcZMwK2yfAUlanch7sQiULpcpxcuneqFLA')
-auth.set_access_token('352639087-GB5VJzsPXWI7Y82wtfaj0AhdIJDfEq1Wy41lcyRa', 'CUDZRLM7rafSNhX4MOw8Vl4Wctqagl0o8gieIT95cydo7')
+def pull_tweets():
+    coordinates = []
+    auth = tweepy.OAuthHandler('rsPQog0xWeNirfT5Bmx9M9CGy', 'S3lmK37dlt7ZNsRIxcZMwK2yfAUlanch7sQiULpcpxcuneqFLA')
+    auth.set_access_token('352639087-GB5VJzsPXWI7Y82wtfaj0AhdIJDfEq1Wy41lcyRa', 'CUDZRLM7rafSNhX4MOw8Vl4Wctqagl0o8gieIT95cydo7')
+    twitter_api = tweepy.API(auth)
+    searchterm="trump"
 
-twitter_api = tweepy.API(auth)
+    result = tweepy.Cursor(twitter_api.search, q=searchterm).items(500)
+
+    for tweet in result: 
+        if tweet.place != None:
+
+            coordinates.append(tweet.place.bounding_box.coordinates[0][0])
+
+    return coordinates
 
 
-searchterm="trump"
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
-result = tweepy.Cursor(twitter_api.search, q=searchterm).items(500)
 
-#for tweet in result: 
-#	if tweet.place != None:
-#		print tweet.text, tweet.place.bounding_box.coordinates[0][0]
 
 
 #write results as csv file
 
 #def tweets(input_filename, output_filename):
-#	with open(inputfilename) as csvfile:
-#		reader = unicodecsv.DictReader(csvfile)
+#   with open(inputfilename) as csvfile:
+#       reader = unicodecsv.DictReader(csvfile)
 
-#		output_rows = []
-#		for row in reader:
-#			out = {
-#				'user': row['user'],
-#				'tweet': row['tweet']
-#				'geolocation': row['geolocation']
-#			}
+#       output_rows = []
+#       for row in reader:
+#           out = {
+#               'user': row['user'],
+#               'tweet': row['tweet']
+#               'geolocation': row['geolocation']
+#           }
 
-
-app.run()
