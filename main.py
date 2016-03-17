51440c8f99c7718396e2a02fd1803e0ef99a9089
Elli Thomas <elli.thomas@gmail.com>
2016-03-17 19:49:03 +0000

    final changes

--- main.py
+++ main.py
@@ -15,36 +15,30 @@ app.debug = True
 
 @app.route("/")
 def hello():
-    coordinates = pull_tweets()
-    return render_template("index.html", templatecoordinates=coordinates)
+    return render_template("index.html", templatecoordinates=[])
 
-
-
-
-
-
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['POST'])
 def sign_up():
     form_data = request.form
-    searchterm = form_data['searchterm']
+    searchterm = form_data['search']
+    coordinates = pull_tweets( searchterm )
+    return render_template("index.html", templatecoordinates=coordinates, searchterm=searchterm)
 
 
    
 
 #define function which sends search term to tweepy to return tweets and geolocation
 
-def pull_tweets():
+def pull_tweets(searchterm):
     coordinates = []
-    auth = tweepy.OAuthHandler('rsPQog0xWeNirfT5Bmx9M9CGy', 'S3lmK37dlt7ZNsRIxcZMwK2yfAUlanch7sQiULpcpxcuneqFLA')
-    auth.set_access_token('352639087-GB5VJzsPXWI7Y82wtfaj0AhdIJDfEq1Wy41lcyRa', 'CUDZRLM7rafSNhX4MOw8Vl4Wctqagl0o8gieIT95cydo7')
+    auth = tweepy.OAuthHandler('tPQSFLn98pEUMb9dsaIWuC8c3', 'lXeDZ0p5ntzO6NDJ5M2AMrORGKg9S5f7I1ooIkOPnghFOxXc7Z')
+    auth.set_access_token('243267961-vf1h9TNE8Dseec9kRuKDeQ5iRHVVQ4FfaPbJyPgh', 'LX80T16mOELwisU9VGo8zEO7syLAUfov2TllMaph8smmi')
     twitter_api = tweepy.API(auth)
-    searchterm="trump"
 
-    result = tweepy.Cursor(twitter_api.search, q=searchterm).items(5000)
+    result = tweepy.Cursor(twitter_api.search, q=searchterm, geocode="51.510523,-0.140592,500km").items(10) # change this back to 1000 at some point
 
     for tweet in result: 
         if tweet.place != None:
-
             coordinates.append(tweet.place.bounding_box.coordinates[0][0])
 
     return coordinates
